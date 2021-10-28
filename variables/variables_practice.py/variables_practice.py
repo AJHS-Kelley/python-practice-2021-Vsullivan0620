57b63f3f3554b7c0f558963c32ba203d29d73cef
Vsullivan0620 <s11268257@students.duvalschools.org>
2021-10-28 14:10:33 -0400

    Update variables_practice.py

--- variables/variables_practice.py/variables_practice.py
+++ variables/variables_practice.py/variables_practice.py
@@ -3,3 +3,9 @@
 # Common Data Types and Variables in Python
 
 lives_remaining = 5 # Interger value, all whole numbers, +/- and 0.
+account_balance = 50000.50 # Floating Point value, +/-.
+user_name = "Sohnny Jins" # String, a collection of letters/numbers.
+
+print(lives_remaining)
+print(account_balance)
+print(user_name)
