0192fedbf0ca822882a0c72047444a80cfcaa33f
Vsullivan0620 <s11268257@students.duvalschools.org>
2021-10-28 14:01:05 -0400

    Create variables_practice.py

---
+++ variables/variables_practice.py/variables_practice.py
@@ -0,0 +1,5 @@
+# Variables Practice, Victor Sullivan, 10/28/21 1:54, version 0.0
+
+# Common Data Types and Variables in Python
+
+lives_remaining = 5 # Interger value, all whole numbers, +/- and 0.
